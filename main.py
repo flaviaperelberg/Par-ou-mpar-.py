895566d315fcd75553b98c3c2c96662f2d78f631
Flávia <113394221+flaviaperelberg@users.noreply.github.com>
2022-09-16 21:53:08 +0000

    Acento.

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # Faça um algoritmo que leia um número inteiro e mostre uma mensagem indicando se este número é par ou ímpar, e se é positivo ou negativo.
 
-numero = int(input('Olá, quer saber se um número inteiro é par ou impar? Quer saber se é positivo ou negativo? Digite o número abaixo e descubra: \n'))
+numero = int(input('Olá, quer saber se um número inteiro é par ou ímpar? Quer saber se é positivo ou negativo? Digite o número abaixo e descubra: \n'))
 
 if int((numero % 2) == 0):
   print('O número é par')
 else:
-  print ('o número é impar')
+  print ('o número é ímpar')
 
 if int((numero >= 0)):
   print ('O número é positivo')
